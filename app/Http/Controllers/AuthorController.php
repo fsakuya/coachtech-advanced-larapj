b2159ccf4060fe108394559df7a62b2305011b31
fsakuya <futamori.1991@gmail.com>
2023-03-09 05:07:13 +0900

    first commit

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Author;
+use Illuminate\Support\Facades\Auth;
+
+class AuthorController extends Controller
+{
+    public function index()
+    {
+        $user = Auth::user();
+        $authors = Author::paginate(4);
+        $param = ['authors' => $authors, 'user' => $user];
+        return view('index', $param);
+    }
+
+    public function check(Request $request)
+    {
+        $text = ['text' => 'ログインして下さい。'];
+        return view('auth', $text);
+    }
+
+    public function checkUser(Request $request)
+    {
+        $email = $request->email;
+        $password = $request->password;
+        if (Auth::attempt([
+            'email' => $email,
+            'password' => $password
+        ])) {
+            $text =   Auth::user()->name . 'さんがログインしました';
+        } else {
+            $text = 'ログインに失敗しました';
+        }
+        return view('auth', ['text' => $text]);
+    }
+}
---
+++ database/seeders/BooksTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Book;
+
+
+class BooksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            'author_id' => 1,
+            'title' => '花より団子'
+        ];
+        Book::create($param);
+    }
+}
